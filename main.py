df9fe28469606c1e0ca1619d80ecae2b3788d681
Sana Parveen V <sanaparveen1829@gmail.com>
2026-01-25 18:42:59 +0530

    Preparing Data Model

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNNClassifier import logger
+from CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
+
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -13,3 +16,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare Base Model Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
---
+++ src/CNNClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,19 @@
+from CNNClassifier.config.configuration import ConfigurationManager
+from CNNClassifier.components.prepare_base_model import PrepareBaseModel
+from CNNClassifier import logger
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+       pass
+
+    def main(self):
+        logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+        config= ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model() 
+        prepare_base_model.update_base_model()
+        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\n")
